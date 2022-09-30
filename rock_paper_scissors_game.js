c105b9e9e214f5fcda8a9625a882324fca680549
Dina Rosita <dina@koudys.com>
2022-09-30 11:23:20 -0400

    Finish RPS game with UI

--- rock_paper_scissors_game.js
+++ rock_paper_scissors_game.js
@@ -1,32 +1,82 @@
+const rpsButtons = document.getElementsByClassName("rpsButton");
+
 const startBox = document.getElementById("startBox");
 const startButton = document.getElementById("startButton");
 const roundBox = document.getElementById("roundBox");
 const roundNumber = document.getElementById("roundNumber");
-const roundMove = document.getElementById("roundMove");
+const moveOption = document.getElementById("moveOption");
 const roundResult = document.getElementById("roundResult");
-const rpsButtons = document.getElementsByClassName("rpsButton");
 const userMove = document.getElementById("userMove");
 const compMove = document.getElementById("compMove");
 const roundMessage = document.getElementById("roundMessage");
 const roundWinner = document.getElementById("roundWinner");
-
 const userScore = document.getElementById("userScore");
 const compScore = document.getElementById("compScore");
+const nextBox = document.getElementById("nextBox");
 const nextRound = document.getElementById("nextRound");
+const gameEnding = document.getElementById("gameEnding");
+const winnerBox = document.getElementById("winnerBox");
+const startOver = document.getElementById("startOver");
 
-
-
-let round = 1;
+let roundNum = 1;
 let totalScore = [0, 0];
+const maxRound = 5;
 
 function startGame() {
     startBox.style = "display: none";
     roundBox.style = "display: block";
-    roundMove.style = "display: block";
-    roundNumber.textContent = "Round " + round;
+    moveOption.style = "display: block";
+    roundNumber.textContent = roundNum;
 }
 
-startButton.addEventListener('click', startGame);
+function playRound(e) {
+    moveOption.style = "display: none";
+    roundResult.style = "display: block";
+
+    const player1 = this.id;
+    const player2 = randomMove();
+
+    userMove.textContent = player1.toUpperCase();
+    compMove.textContent = player2.toUpperCase();
+
+    const roundScore = getScore(player1, player2);
+    totalScore[0] = totalScore[0] + roundScore[0];
+    totalScore[1] = totalScore[1] + roundScore[1];
+
+    roundMessage.textContent = getMessage(roundScore, player1, player2);
+    roundWinner.textContent = getWinner(roundScore);
+    userScore.textContent = roundScore[0];
+    compScore.textContent = roundScore[1];
+    userTotal.textContent = totalScore[0];
+    compTotal.textContent = totalScore[1];
+
+    if (roundNum === maxRound) {
+        winnerBox.textContent = getWinner(totalScore);
+        nextBox.style = "display: none";
+        gameEnding.style = "display: block";    
+    }
+}
+
+function playNext() {
+    moveOption.style = "display: block";
+    roundResult.style = "display: none";
+    roundNum += 1;
+    roundNumber.textContent = roundNum;
+}
+
+
+function refreshPage(){
+    startBox.style = "display: block";
+    nextBox.style = "display: block";
+
+    roundBox.style = "display: none";
+    moveOption.style = "display: none";
+    roundResult.style = "display: none";
+    gameEnding.style = "display: none";
+    
+    roundNum = 1;
+    totalScore = [0, 0];
+}
 
 function randomMove() {
     let randomNum = Math.floor(Math.random()*3 + 1);
@@ -39,28 +89,14 @@ function randomMove() {
     }
 }
 
-function playRound(e) {
-    roundMove.style = "display: none";
-    roundResult.style = "display: block";
-
-    const player1 = this.id;
-    const player2 = randomMove();
-
-    userMove.textContent = player1.toUpperCase();
-    compMove.textContent = player2.toUpperCase();
-
-    const score = getScore(player1, player2);
-    totalScore[0] = totalScore[0] + score[0];
-    totalScore[1] = totalScore[1] + score[1];
-
-    const comment = getMessage(score, player1, player2);
-    roundMessage.textContent = comment[0];
-    roundWinner.textContent = comment[1];
-    userScore.textContent = score[0];
-    compScore.textContent = score[1];
-    userTotal.textContent = totalScore[0];
-    compTotal.textContent = totalScore[1];
-
+function getWinner(score) {
+    if (score[0] > score[1]) {
+        return "You win!";
+    } if (score[0] < score[1]) {
+        return "You lose!";
+    } else {
+        return "It's a tie!";
+    }
 }
 
 function getVerb(item) {
@@ -70,16 +106,6 @@ function getVerb(item) {
         return "wraps";
     } else if (item === "scissors") {
         return "cuts";
-    }
-}
-
-function getMessage(score, p1, p2) {
-    if (score[0] === 1) {
-        return [`Your ${p1} ${getVerb(p1)} computer's ${p2}.`, `You win!`];
-    } else if (score[1] === 1) {
-        return [`Computer's ${p2} ${getVerb(p2)} your ${p1}. `, `Computer wins!`];
-    } else {
-        return [`No winner here...`, `It's a tie!`];
     }
 }
 
@@ -101,68 +127,21 @@ function getScore(p1, p2) {
     }
 }
 
-function randomMove() {
-    let randomNum = Math.floor(Math.random()*3 + 1);
-    if (randomNum === 1) {
-        return "rock";
-    } else if (randomNum === 2) {
-        return "paper";
-    } else if (randomNum === 3) {
-        return "scissors";
+function getMessage(score, p1, p2) {
+    if (score[0] === 1) {
+        return `Your ${p1} ${getVerb(p1)} computer's ${p2}.`;
+    } else if (score[1] === 1) {
+        return `Computer's ${p2} ${getVerb(p2)} your ${p1}. `;
+    } else {
+        return `Nothing really happens...`;
     }
 }
 
+startButton.addEventListener('click', startGame);
 Array.from(rpsButtons).forEach(bt => bt.addEventListener("click", playRound));
-
-function playNext() {
-    scoreBox.style = "display: block";
-    // update summary
-}
-
 nextRound.addEventListener('click', playNext);
-
-
-// function playRound (e) {
-//     const comp = randomMove();
-//     const user = this.id;
-//     const score = getScore(user, comp);
-//     const message = getMessage(score, user, comp);
-//     totalRun += 1;
-//     totalScore += score;
-
-
-
-//     roundMessage.style = "display: block;"
-//     roundMessage.textContent = message;
-// }
-
-    
+startOver.addEventListener('click', refreshPage);
 
 
 
 
-
-
-
-// const roundMessage = document.getElementById("roundMessage");
-// let totalScore = 0;
-// let totalRun = 0;
-
-
-
-
-
-
-//
-
-
-
-// // function game() {
-// //     for (let i = 0; i < 5; i++) {
-// //         const playerSelection = prompt("rock, paper, scissors?");
-// //         const computerSelection = randomMove();
-// //         console.log(playRound(playerSelection, computerSelection));
-// //     }
-// // }
-
-// // game();
